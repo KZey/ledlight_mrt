0aeede83a609e937cfcc92a5635eb3ef3281c9a3
Zhao Peng <keith_cn@hotmail.com>
2013-08-20 11:23:25 -0700

    sns: tweet a listing

--- protected/views/property/view.php
+++ protected/views/property/view.php
@@ -164,7 +164,7 @@ var mygallery2=new simpleGallery({
 						<?php if($model->non_listing != 1){?>
 						<div style="border:0px solid red;margin:5px 0 0 5px;">
 							<img src="/images/blue_bg.png" border=0 /><br/>
-							<a href="https://twitter.com/share" class="twitter-share-button" count="none" data-lang="en">Twitter</a>
+							<a href="https://twitter.com/intent/tweet?text=Check out this property listing on MyRealTour.com: http://myrealtour.com/property/<?php echo $model->property_id;?>" class="twitter-share-button" count="none" data-lang="en">11111Twitter</a>
 <script>!function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0];if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src="https://platform.twitter.com/widgets.js";fjs.parentNode.insertBefore(js,fjs);}}(document,"script","twitter-wjs");</script>
 <div class="fb-like" data-send="false" data-layout="button_count" data-width="450" data-show-faces="false"></div>
 						</div>
